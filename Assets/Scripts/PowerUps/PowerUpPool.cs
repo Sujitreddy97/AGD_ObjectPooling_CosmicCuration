96c678f3b7b22b22ef83c1a9cfc26318f4d5f938
Sujit Reddy <Sujitreddy97@users.noreply.github.com>
2024-06-11 15:53:47 +0530

    Created PowerUpPool
    - Written logic such that this pool can handle different types of powerups

---
+++ Assets/Scripts/PowerUps/PowerUpPool.cs
@@ -0,0 +1,39 @@
+﻿using CosmicCuration.PowerUps;
+using CosmicCuration.Utilities;
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.PowerUps
+{
+    public class PowerUpPool : GenericObjectPool<PowerUpController>
+    {
+        private PowerUpData powerUpData;
+
+        public PowerUpController GetPowerUp<T>(PowerUpData powerUpData) where T : PowerUpController
+        {
+            this.powerUpData = powerUpData;
+            return GetItem<T>();
+        }
+
+        protected override PowerUpController CreateItem<T>()
+        {
+            if (typeof(T) == typeof(Shield))
+            {
+                return new Shield(powerUpData);
+            }
+            else if (typeof(T) == typeof(RapidFire))
+            {
+                return new RapidFire(powerUpData);
+            }
+            else if (typeof(T) == typeof(DoubleTurret))
+            {
+                return new DoubleTurret(powerUpData);
+            }
+            else
+            {
+                throw new NotSupportedException("Power Up Type Not Supported");
+            }
+        }
+    }
+}
